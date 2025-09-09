e43a35ce5e28b1f38893a40ed6e94363b00c5033
MohamadAbudraiea <mohamadabudraiea2003@outlook.com>
2025-09-09 22:09:01 +0300

    finished auth

--- backend/routers/authRouter.js
+++ backend/routers/authRouter.js
@@ -2,10 +2,12 @@ const express = require("express");
 const router = express.Router();
 const sharedController = require("../controllers/shared");
 const authController = require("../controllers/authController");
+const authUser = require("../middlewares/authentication");
+const authenticateUser = require("../middlewares/authentication");
 //--------------------------------------------
 router.post("/login", authController.userLogin);
 router.post("/signup", authController.userSignup);
 router.post("/logout", authController.logout);
 //--------------------------------------------
-
+router.get("/check", authenticateUser, authController.check);
 module.exports = router;
